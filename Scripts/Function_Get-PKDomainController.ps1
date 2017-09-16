643ba92e38d83c5cd128b36665cb3143b838784a
Paula Kingsley <lanwench@users.noreply.github.com>
2017-09-16 09:40:10 -0700

    v1.3.0

---
+++ Scripts/Function_Get-PKDomainController.ps1
@@ -0,0 +1,235 @@
+﻿#requires -Version 3
+Function Get-PKADDomainController {
+<#
+
+.Synopsis
+    Returns a domain controller for the current computer site or a named site in the current or named domain
+    
+.DESCRIPTION
+   Returns a domain controller for the current computer site or a named site in the current or named domain
+   If the first match fails, tries adjacent sites; if that fails, returns the first DC in the domain
+   Uses .NET System.DirectoryServices.ActiveDirectory.DirectoryContext (no ActiveDirectory module needed)
+   Returns a DomainController object (System.DirectoryServices.ActiveDirectory.DirectoryServer)
+   
+.NOTES
+    Name    : Function_Get-PKADDomainController.ps1
+    Created : 2017-09-15
+    Version : 01.00.0000
+    Author  : Paula Kingsley
+    
+    History:
+
+        ** PLEASE KEEP $VERSION UPDATED IN PROCESS BLOCK *
+
+        v01.00.0000 - 2017-09-15 - Created script
+        
+.EXAMPLE
+    PS C:\> Get-PKDomainController -Verbose
+
+        VERBOSE: PSBoundParameters: 
+	
+        Key           Value                  
+        ---           -----                  
+        Verbose       True                   
+        ADDomain      domain.local
+        ADSite        Las Vegas             
+        ScriptName    Get-PKDomainController 
+        ScriptVersion 1.0.0                  
+
+        VERBOSE: Get domain details
+        VERBOSE: Domain 'domain.local' is running in Windows2008R2Domain mode in forest 'domain.local'
+        VERBOSE: Get computer site
+        VERBOSE: Current site is 'Las Vegas'
+        VERBOSE: Get domain controllers in site
+        VERBOSE: Found domain controller 'DC08.domain.local'
+
+
+        Forest                     : domain.local
+        CurrentTime                : 2017-09-15 20:45:37
+        HighestCommittedUsn        : 162461849
+        OSVersion                  : Windows Server 2008 R2 Enterprise
+        Roles                      : {}
+        Domain                     : domain.local
+        IPAddress                  : 10.15.144.11
+        SiteName                   : Las Vegas
+        SyncFromAllServersCallback : 
+        InboundConnections         : {026fc520-6d52-4d33-9c61-b2b8950e270a, 1bde646a-2f1f-4f66-b8b5-7ac544f4637e, 
+                                     1f3b6863-d5ab-4a86-8b9a-6ec78fdbfe5e, 6ed28c8c-ca6c-4556-900c-b137b5b94ed2...}
+        OutboundConnections        : {6c98b0bc-83fe-4ce7-85de-22bc4e46d442}
+        Name                       : DC08.domain.local
+        Partitions                 : {DC=domain,DC=local, CN=Configuration,DC=domain,DC=local, 
+                                     CN=Schema,CN=Configuration,DC=domain,DC=local, 
+                                     DC=DomainDnsZones,DC=domain,DC=local...}
+.EXAMPLE
+    PS C:\> Get-PKDomainController -ADSite Sacramento -Verbose
+
+        VERBOSE: PSBoundParameters: 
+	
+        Key           Value                  
+        ---           -----                  
+        ADSite        Sacramento             
+        Verbose       True                   
+        ADDomain      gracenote.gracenote.com
+        ScriptName    Get-PKDomainController 
+        ScriptVersion 1.0.0                  
+
+
+
+        VERBOSE: Get domain details
+        VERBOSE: Domain 'gracenote.gracenote.com' is running in Windows2008R2Domain mode in forest 'gracenote.gracenote.com'
+        VERBOSE: Get site
+        VERBOSE: Current site is 'Sacramento'
+        VERBOSE: Get domain controller in site
+        VERBOSE: Found domain controller 'SACDC01.gracenote.gracenote.com'
+
+
+        Forest                     : gracenote.gracenote.com
+        CurrentTime                : 2017-09-15 20:51:56
+        HighestCommittedUsn        : 588463910
+        OSVersion                  : Windows Server 2008 R2 Enterprise
+        Roles                      : {SchemaRole, NamingRole, PdcRole, RidRole...}
+        Domain                     : gracenote.gracenote.com
+        IPAddress                  : 10.8.142.103
+        SiteName                   : Sacramento
+        SyncFromAllServersCallback : 
+        InboundConnections         : {6ba69348-a596-4f09-b1a4-652e44b6fea0, EVLDC00, GFDC01, SCDC03}
+        OutboundConnections        : {198b36d4-2bbb-4e76-9079-e9cc53c9196f}
+        Name                       : SACDC01.gracenote.gracenote.com
+        Partitions                 : {DC=gracenote,DC=gracenote,DC=com, CN=Configuration,DC=gracenote,DC=gracenote,DC=com, 
+                                     CN=Schema,CN=Configuration,DC=gracenote,DC=gracenote,DC=com, 
+                                     DC=DomainDnsZones,DC=gracenote,DC=gracenote,DC=com...}
+
+
+
+#>
+[CmdletBinding()]
+Param (
+    [Parameter(
+        Mandatory = $False,
+        HelpMessage = "AD domain name FQDN (default is current computer)"
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string]$ADDomain = [System.DirectoryServices.ActiveDirectory.Domain]::GetComputerDomain(),
+
+    [Parameter(
+        Mandatory = $False,
+        HelpMessage = "AD site (default is current computer)"
+    )]
+    [ValidateNotNullOrEmpty()]
+    [string]$ADSite = [System.DirectoryServices.ActiveDirectory.ActiveDirectorysite]::GetComputerSite()
+
+)
+Begin {
+
+    
+    # Current version (please keep up to date from comment block)
+    [version]$Version = "01.00.0000"
+
+    # Show our settings
+    $CurrentParams = $PSBoundParameters
+    $MyInvocation.MyCommand.Parameters.keys | Where {$CurrentParams.keys -notContains $_} | 
+        Where {Test-Path variable:$_}| Foreach {
+            $CurrentParams.Add($_, (Get-Variable $_).value)
+        }
+    $CurrentParams.Add("ScriptName",$MyInvocation.MyCommand.Name)
+    $CurrentParams.Add("ScriptVersion",$Version)
+    Write-Verbose "PSBoundParameters: `n`t$($CurrentParams | Format-Table -AutoSize | out-string )"
+
+    # Preference
+    $ErrorActionPreference = "Stop"
+
+}
+
+Process {
+    
+    $Msg = "Get domain details"
+    Write-Verbose $Msg
+
+    $Activity = "Get domain controller in site"
+    $CurrentOperation = $Msg
+    Write-Progress -Activity $Activity -CurrentOperation $CurrentOperation
+
+    Try {
+        $DomainType = [System.DirectoryServices.ActiveDirectory.DirectoryContexttype]"Domain"
+        $Context = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext($DomainType, $ADDomain)
+        $DomainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($Context)
+            
+        $Msg = "Domain '$($DomainObj.Name)' is running in $($DomainObj.DomainMode) mode in forest '$($DomainObj.Forest)'"
+        Write-Verbose $Msg
+
+    }
+    Catch {
+        $Msg = "Operation failed for domain '$ADDomain'"
+        $ErrorDetails = ($_.Exception.Message.Split(':')[1]).Trim().Replace('"','').Replace('.','')
+        $Host.UI.WriteErrorLine("ERROR: $Msg`n$ErrorDetails")
+        Break
+    }
+        
+
+    $Msg = "Get site"
+    Write-Verbose $Msg
+    $CurrentOperation = $Msg
+    Write-Progress -Activity $Activity -CurrentOperation $CurrentOperation
+
+    Try {
+        $SiteContext = new-object System.DirectoryServices.ActiveDirectory.DirectoryContext("Forest", $DomainObj.Forest)
+        If (-not ($SiteObj = [System.DirectoryServices.ActiveDirectory.Forest]::GetForest($SiteContext).sites | Where-Object {$_.Name -eq $ADSite})){
+            $Msg = "Invalid AD site '$ADSite"
+            $Host.UI.WriteErrorLine("ERROR: $Msg")
+            Break
+        }
+        Else {
+            $SiteName = $SiteObj.Name
+            $Msg = "Current site is '$SiteName'"
+            Write-Verbose $Msg
+        }
+    }
+    Catch {
+        $Msg = "Operation failed for site '$ADSite'"
+        $ErrorDetails = ($_.Exception.Message.Split(':')[1]).Trim().Replace('"','').Replace('.','')
+        $Host.UI.WriteErrorLine("ERROR: $Msg`n$ErrorDetails")
+        Break
+            
+    }
+    
+    $Msg = "Get domain controller in site"
+    Write-Verbose $Msg
+    $CurrentOperation = $Msg
+    Write-Progress -Activity $Activity -CurrentOperation $CurrentOperation
+
+    Try {
+        If (-not ($DC = $DomainObj.DomainControllers | Where-Object {$_.SiteName -eq $SiteName} | Select -first 1)) {
+            $Msg = "Failed to get domain controller for '$SiteName'; check connected sites"
+            Write-Warning $Msg
+            $SiteObj.AdjacentSites | Foreach-Object {
+                $SiteName = $_.Name
+                If (-not ($DC = $DomainObj.DomainControllers | Where-Object {$_.SiteName -eq $SiteName} | Select -first 1)) {
+                    $Msg = "Failed to get domain controller for '$SiteName'; select first DC in domain"
+                    Write-Warning $Msg
+                    $DC = $DomainObj.DomainControllers[0]
+                }
+            } #end foreach adjacent site
+        } # end get DC in site
+
+        If ($DC) {
+            $Msg = "Found domain controller '$($DC.Name)'"
+            Write-Verbose $Msg
+            Write-Output $DC
+        }
+        Else {
+            $Msg = "No domain controller found"
+            $Host.UI.WriteErrorLine("ERROR: $Msg")
+        }       
+    }
+    Catch {
+        $Msg = "Domain controller lookup failed"
+        $ErrorDetails = $_.Exception.Message
+        $Host.UI.WriteErrorLine("ERROR: $Msg`n$ErrorDetails")
+    }
+
+    Write-Progress -Activity $Activity -Completed
+}
+} #end Get-PKADDomainController
+
+
+
---
+++ Scripts/Function_Test-PKWindowsPendingReboot.ps1
@@ -0,0 +1,301 @@
+﻿
+#requires -Version 3
+function Test-PKWindowsPendingReboot {
+<#
+.SYNOPSIS
+    Invokes a PSJob using Invoke-Command to test the pending reboot status of a computer
+    
+.DESCRIPTION
+    Invokes a PSJob using Invoke-Command to test the pending reboot status of a computer
+    Optionally performs a WinRM connection test before attempting job invocation
+    Accepts pipeline input
+    Output is a PSObject or boolean
+    Returns a PSJob
+    
+
+.NOTES
+    Name    : Function_Test-PKWindowsPendingReboot.ps1
+    Version : 01.00.0000
+    Author  : Paula Kingsley
+    Created : 2016-09-16
+    History :
+
+        ** PLEASE KEEP $VERSION UPDATED IN BEGIN BLOCK **
+
+        v01.00.0000 - 2017-09-12 - Created script based on Monimoy Sanyal's original (see gallery link)
+
+.LINK
+    https://gist.github.com/altrive/5329377
+
+.LINK
+    http://gallery.technet.microsoft.com/scriptcenter/Get-PendingReboot-Query-bdb79542
+
+#>
+[Cmdletbinding(
+    SupportsShouldProcess = $True,
+    ConfirmImpact = "Medium"
+)]
+Param(
+
+    [Parameter(
+        Mandatory=$False,
+        Position = 0,
+        ValueFromPipeline=$True,
+        ValueFromPipelineByPropertyName=$True,
+        HelpMessage="Name of computer (separate multiple computers with commas)"
+    )]
+    [Alias("FQDN","HostName","Computer","VM","DNSDomainName","Name")]
+    [ValidateNotNullOrEmpty()]
+    [String[]] $ComputerName = $Env:ComputerName,
+
+    [Parameter(
+        Mandatory=$False,
+        HelpMessage="Valid admin credentials on target computer"
+    )]
+    [ValidateNotNullOrEmpty()]
+    [pscredential] $Credential = [System.Management.Automation.PSCredential]::Empty,
+
+    [Parameter(
+        Mandatory=$False,
+        HelpMessage="Return boolean output only"
+    )]
+    [switch]$BooleanOutput
+)
+    
+Begin {
+
+    # Current version (please keep up to date from comment block)
+    [version]$Version = "1.00.0000"
+
+    # Show our settings
+    $CurrentParams = $PSBoundParameters
+    $MyInvocation.MyCommand.Parameters.keys | Where {$CurrentParams.keys -notContains $_} | 
+        Where {Test-Path variable:$_}| Foreach {
+            $CurrentParams.Add($_, (Get-Variable $_).value)
+        }
+    $CurrentParams.Add("ScriptName",$MyInvocation.MyCommand.Name)
+    $CurrentParams.Add("ScriptVersion",$Version)
+    Write-Verbose "PSBoundParameters: `n`t$($CurrentParams | Format-Table -AutoSize | out-string )"
+
+    # Preferences 
+    $ErrorActionPreference = "Stop"
+    $ProgressPreference = "Continue"
+
+    # General purpose splat
+    $StdParams = @{}
+    $StdParams = @{
+        ErrorAction = "Stop"
+        Verbose     = $False
+    }
+    
+    # Scriptblock for invoke-command
+    $Scriptblock = {
+        
+        Param($BooleanOutput)
+
+        # Define variables
+        [switch]$Bool = $Using:BooleanOutput
+        $ErrorActionPreference = "Stop"
+        
+        # Output object, not an ordered hashtable for backwards compatibility
+        # so select array ensures property order
+        $InitialValue = "Error"
+        $Output = New-Object PSObject -Property @{
+            ComputerName                         = $Env:ComputerName
+            IsRebootPending                      = $InitialValue
+            ComponentBasedServicingRebootPending = $InitialValue
+            WindowsUpdateRebootPending           = $InitialValue
+            PendingFileRenameOperation           = $InitialValue
+            #SCCM                                 = $InitialValue
+            Messages                             = $InitialValue
+        }            
+        
+        If ($Bool.IsPresent) {$Select = "IsRebootPending"}
+        Else {$Select = "ComputerName","IsRebootPending","ComponentBasedServicingRebootPending","PendingFileRenameOperation","PendingFileRenameOperation","Messages"}
+
+        $Pending = 0
+        $ErrMsg = @()
+
+        #Test Component Based Servicing Reboot Pending
+        Try {
+            If (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA SilentlyContinue) { 
+                $Pending ++
+                $Output.ComponentBasedRebootPending = $True
+            }
+            Else {
+                $Output.ComponentBasedRebootPending = $False
+            }
+        }
+        Catch {
+            $ErrMsg += $_.Exception.Message
+        }
+    
+        # Test Windows Update Reboot Pending
+        Try {
+            If (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA SilentlyContinue) { 
+                $Pending ++
+                $Output.WindowsUpdatePending = $True
+            }
+            Else {
+                $Output.WindowsUpdatePending = $False
+            }
+        }
+        Catch {
+            $ErrMsg += $_.Exception.Message
+        }
+
+        # Test Pending File Rename Operation
+        Try {
+            If (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA SilentlyContinue) { 
+                $Pending ++
+                $Output.PendingFileRenameOperation = $True
+            }
+            Else {
+                $Output.PendingFileRenameOperation = $False
+            }
+        }
+        Catch {
+            $ErrMsg += $_.Exception.Message
+        }
+
+        <#
+        # SCCM (not currently in use)    
+        Try { 
+           $SCCMClientUtil = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities" 
+           $SCCMStatus = $SCCMClientUtil.DetermineIfRebootPending()
+
+           If (($SCCMStatus -ne $Null) -and $SCCMStatus.RebootPending){
+                $Pending ++
+                $Output.SCCM = $True
+           }
+         }
+         Catch{
+            $ErrMsg += $_.Exception.Message
+         }
+         #>
+
+         If ($Pending.Count -gt 0) {$Output.IsPendingReboot = $True}
+         Else {$Output.IsPendingReboot = $False}
+
+         # Return the output object
+         Return ($Output | Select $Select)    
+        
+    } #end scriptblock for remote command
+
+    # Splat for test-wsman
+    $Param_WSMan = @{}
+    $Param_WSMan = @{
+        ComputerName   = ""
+        Authentication = "Negotiate"
+        Credential     = $Credential
+        ErrorAction    = "SilentlyContinue"
+        Verbose        = $False
+    }
+
+    # Splat for invoke-command
+    $Param_IC = @{}
+    $Param_IC = @{
+        ComputerName = ""
+        Credential   = $Credential
+        Scriptblock  = $Scriptblock
+        ArgumentList = $BooleanOutput
+        AsJob        = $True
+        JobName      = ""
+        ErrorAction  = "Stop"
+        Verbose      = $False
+    }
+
+    # Splat for write-progress
+    If ($SkipConnectionTest.IsPresent) {$Activity = "Submit job to test pending reboot state"}
+    Else {$Activity = "Test connection and submit job to test pending reboot state"}
+    $Param_WP = @{}
+    $Param_WP = @{
+        Activity         = $Activity
+        PercentComplete  = ""
+        CurrentOperation = "Working"
+        Status           = "Working"
+    }
+
+    # Output arrays
+    $Jobs = @()
+
+}
+Process {    
+
+    # for progress bar    
+    $Total = $ComputerName.Count
+    $Current = 0
+        
+    # Loop through the array
+    Foreach ($Computer in $ComputerName) {
+                
+        $Current ++
+        [switch]$Continue = $False
+
+        $Param_WP.CurrentOperation = $Computer
+        $Param_WP.PercentComplete = (($Current/$Total) * 100) 
+        $Param_WSMan.ComputerName = $Computer
+
+        Write-Verbose $Computer
+        Write-Progress @Param_WP
+
+        #If ($PSCmdlet.ShouldProcess($Computer,$Activity)) {
+        If ($Computer) {
+
+            If (-not $SkipConnectionTest.IsPresent) {
+            
+                If ($Null = Test-WSMan @Param_WSMan ) {$Continue = $True}
+                Else {
+                    $Msg = "Connection failure on $Computer"
+                    $Host.UI.WriteErrorLine("ERROR: $Msg")
+                    $Continue = $False
+                }
+
+            } #end if testing WinRM
+
+            Else {$Continue = $True}
+            
+            If ($Continue.IsPresent) {
+                
+                Try {
+                    $Time = (Get-Date).ToString()
+                    $Param_IC.ComputerName = $Computer
+                    $Param_IC.JobName = "TestReboot_$Computer`_$Time"
+                    $Job = Invoke-Command @Param_IC
+                    $Jobs += $Job
+                    $Msg = "Job ID $($Job.ID): $($Job.Name)"
+                    Write-Verbose $Msg
+                }
+
+                Catch {
+                    $Msg = "Remote job creation failed for $Computer"
+                    $ErrorDetails = $_.Exception.Message
+                    $Host.UI.WriteErrorLine("ERROR: $Msg`n$ErrorDetails")
+                    Continue
+                }
+            } #end if continue
+               
+        } #end if confirmed
+        Else {
+            $Msg = "Job cancelled by user on $Computer"
+            $Host.UI.WriteErrorLine($Msg)
+        } #end if cancelled
+
+    } #end create job for each computer
+    
+    $Null = Write-Progress -Activity $Activity -Completed -ErrorAction SilentlyContinue -Verbose:$False
+
+}
+End {
+
+    If ($Jobs.Count -gt 0) {
+        $Msg = "$($Jobs.Count) job(s) created; run 'Get-Job -Id # | Wait-Job | Receive-Job' to view output"
+        Write-Verbose $Msg
+        $Jobs | Get-Job
+    }
+    Else {
+        $Msg = "No jobs created"
+        $Host.UI.WriteErrorLine($Msg)
+    }
+}
+}
